d954ef91fc91ba7272b050cb13b9f5d3bd0c7942
Ayami00 <I79606606960@yandex.ru>
2022-03-05 14:16:22 +0300

    v2.0

--- Задание№4/№2.py
+++ Задание№4/№2.py
@@ -1,2 +1,16 @@
 # Создать текстовый файл (не программно), сохранить в нем несколько строк,
 # выполнить подсчет количества строк, количества слов в каждой строке
+
+with open("#2.txt", 'w') as file_obj:
+    file = ['1 stroka\n', '2 stroka\n', '3 stroka\n', '4 stroka\n', '5 stroka\n']
+    file_obj.writelines(file)
+
+with open("#2.txt") as file_obj:
+    stroki = 0
+    simvoli = 0
+    for line in file_obj:
+        stroki += line.count("\n")
+        simvoli = len(line)-1
+        print(f"{simvoli} символов в строке")
+
+    print(f"Всего строк: {stroki}")
