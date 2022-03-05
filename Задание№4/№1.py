9f51030622842da7a28b95e284533daa2ead2083
Ayami00 <I79606606960@yandex.ru>
2022-03-05 13:51:33 +0300

    v2.0

--- Задание№4/№1.py
+++ Задание№4/№1.py
@@ -1,3 +1,16 @@
 # Создать программно файл в текстовом формате, записать в него построчно
 # данные, вводимые пользователем. Об окончании ввода данных свидетельствует
 # пустая строка.
+file = []
+with open("#1.txt", "w") as file_obj:
+    file_obj.writelines(file)
+while True:
+    stroka = input("Введите текст: ")
+    if stroka == '':
+        print(file)
+        exit()
+    else:
+        nstroka = stroka + '\n'
+        file.append(nstroka)
+
+
